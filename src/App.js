9219f056730a2fb1347d00fd0e8ea1762c9d1729
Danny Diekroeger <dannydiek@gmail.com>
2023-03-29 17:19:49 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Modal from 'react-bootstrap/Modal'
-import { SocialIcon } from 'react-social-icons';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { TailSpin } from 'react-loading-icons'
 import { openai } from '@deezy-inc/web-ai'
 
 import Container from 'react-bootstrap/Container';
 import './App.css';
-const axios = require('axios')
 
+const messages = [{
+  "role": "system",
+  "content": "You are a custom ChatGPT on WebAI Demo gated by bitcoin lightning micropayments"
+}]
 const App = () => {
-  const [index, setIndex] = useState({})
-  const [showGenerateModal, setShowGenerateModal] = useState(false)
-  const [currentGlpyh, setCurrentGlyph] = useState('')
-
-  useEffect(() => {
-    
-  }, [])
-
+  const [responseText, setResponseText] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
   return (
     <>
-      <Navbar id="top-nav" className="pt-3 pb-3 text-center" expand="lg">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav id="top-social-links">
-              <Nav.Link onClick={() => window.open(GITHUB_URL)}>
-                <SocialIcon url={GITHUB_URL} />
-              </Nav.Link>
-              <Nav.Link onClick={() => window.open(DISCORD_URL)}>
-                <SocialIcon url={DISCORD_URL} />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <Container className="text-center">
-        <h1 className="py-3">WebAI Demo</h1>
-        <Form>
-          <Form.Label>Enter prompt</Form.Label>
-          <Form.Text></Form.Text>
-        </Form>
-        <Button variant="primary" className="shadowed-orange-small mt-5" onClick={() => {
-          console.log('click')
+        <h1 className="py-3 my-5">WebAI Demo</h1>
+        <Form.Label className="mb-4">Enter a prompt for ChatGPT:</Form.Label>
+        <Form.Control
+          type="text"
+          id="promptInput"
+          aria-describedby="promptInput"
+          className="m-auto"
+          placeholder="Tell me a fun fact"
+          style={{ maxWidth: '600px', textAlign: 'center' }}
+        />
+        <Button variant="primary" className="shadowed-orange-small mt-4 mb-5" disabled={isLoading} onClick={async () => {
+          setResponseText('')
+          setIsLoading(true)
+          messages.push({
+            "role": "user",
+            "content": document.getElementById('promptInput').value
+          })
+          const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages
+          })
+          setIsLoading(false)
+          messages.push(response.choices[0].message)
+          console.log(messages)
+          setResponseText(response.choices[0].message.content)  
         }}>
-          Submit<br />
+          ⚡ Submit ⚡<br />
         </Button>
+        <br />
+        { isLoading ? <TailSpin /> : <></>}
+        <p style={{ maxWidth: '600px', textAlign:'center', margin:'auto'}}>
+          {responseText ? <>
+            {responseText}
+          </>
+          :
+          <></>
+        }
+        </p>
       </Container>
     </>
   )
